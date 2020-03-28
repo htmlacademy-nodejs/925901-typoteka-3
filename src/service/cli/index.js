d0e0bcca5a1de75f2881207a0cfcee355a8f9c04
Dmitrii Halimov <dmitrii.halimov@siteminder.com>
2020-03-28 17:04:46 +0200

    feat(service/cli): Add --version command

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const help = require(`./help`);
+const version = require(`./version`);
 
 const Cli = {
-  [help.name]: help
+  [help.name]: help,
+  [version.name]: version,
 };
 
 module.exports = {
