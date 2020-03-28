3600461b13c80755deaa42def131ed6d9ba387d2
Dmitrii Halimov <dmitrii.halimov@siteminder.com>
2020-03-28 20:29:46 +0200

    feat(service/cli): Add --generate command

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -2,10 +2,12 @@
 
 const help = require(`./help`);
 const version = require(`./version`);
+const generate = require(`./generate`);
 
 const Cli = {
   [help.name]: help,
   [version.name]: version,
+  [generate.name]: generate,
 };
 
 module.exports = {
